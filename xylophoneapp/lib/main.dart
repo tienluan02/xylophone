742e6717fbce3de7c6ec6dd5af3bc3192bbb95c5
tienluandev <nguyentienluan3@gmail.com>
2024-08-09 09:43:29 +0700

    Initial commit

---
+++ xylophoneapp/lib/main.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.green,
+        appBar: AppBar(
+          backgroundColor: Colors.blue[800],
+          title: const Center(
+              child: Text(
+                'Music timee',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30.0,
+                  color: Colors.black,
+                ),
+              )
+          ),
+        ),
+        body: const MusicPage(),
+      ),
+    );
+  }
+}
+
+class MusicPage extends StatefulWidget {
+  const MusicPage({super.key});
+
+  @override
+  State<MusicPage> createState() => _MusicPageState();
+}
+
+class _MusicPageState extends State<MusicPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(child: TextButton(
+                onPressed: () {},
+                child: Text(
+                  'hihi',
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              ),
+      ]
+          ),
+          Expanded(child: TextButton(
+            onPressed: () {},
+            child: Text(
+              'hihi',
+              style: TextStyle(
+                color: Colors.black,
+              ),
+            ),
+          ),
+          ),
+          Expanded(child: TextButton(
+            onPressed: () {},
+            child: Text(
+              'hihi',
+              style: TextStyle(
+                color: Colors.black,
+              ),
+            ),
+          ),
+          ),
+        ],
+      ),
+    );
+  }
+}
